8c1f8351ef9d4add92a59fc0c12ba0cc679b745d
DreadkingRathalos <108301431+DreadkingRathalos@users.noreply.github.com>
2022-07-20 14:17:04 -0500

    Create Locale_Information.py

---
+++ Locale_Information.py
@@ -0,0 +1,16 @@
+Number=input('Please Enter a number to learn a Locale')
+if(Number == 1):
+  Locale = 
+  Locale_Info1 =
+  Locale_Info2 =
+  Locale_Info3 =
+  
+
+  
+  
+  
+  
+  print(Locale)
+print(Locale_Info1)
+print(Locale_Info2)
+print(Locale_Info3)
