a33bc57e1c6bd2fb1ab8b04724188462f8638bf7
DreadkingRathalos <108301431+DreadkingRathalos@users.noreply.github.com>
2022-07-20 14:47:21 -0500

    Update and rename monsterhunterrisedata.py to Monster_Info.py

---
+++ Monster_Info.py
@@ -0,0 +1,114 @@
+#Monster Hunter Rise Sunbreak is owned by Capcom
+print('Welcome!')
+MonsterData=input('Please Enter a number to identify a Monster.')
+if(MonsterData=1)
+    Monster_Name = 'Monster: Lurker Legiana'
+    Elements = 'Elements: Dragon'
+    Aliments = 'Aliments: Dragonblight, ' 
+    Weaknesses = 'Weaknesses: Thunder' 
+    Breakable_Parts = 'Breakable Parts: Head, Wings, Tail'
+    Rare_Materials = 'Rare Materials: Lurker Gem, Lurker Armor'
+    Locale = 'Habitats: Citadel'
+    Biology1 = 'A l'
+    Biology2 = ''
+    Biology3 = ''
+if(MonsterData=2)
+    Monster_Name = 'Monster: '
+    Elements = 'Elements: '
+    Aliments = 'Aliments: ' 
+    Weaknesses = 'Weaknesses: ' 
+    Breakable_Parts = 'Breakable Parts: '
+    Rare_Materials = 'Rare Materials: '
+    Biology1 = ''
+    Biology2 = ''
+    Biology3 = ''    
+if(MonsterData=3)
+    Monster_Name = 'Monster: '
+    Elements = 'Elements: '
+    Aliments = 'Aliments: ' 
+    Weaknesses = 'Weaknesses: ' 
+    Breakable_Parts = 'Breakable Parts: '
+    Rare_Materials = 'Rare Materials: '
+    Biology1 = ''
+    Biology2 = ''
+    Biology3 = ''
+if(MonsterData=4)
+    Monster_Name = 'Monster: '
+    Elements = 'Elements: '
+    Aliments = 'Aliments: ' 
+    Weaknesses = 'Weaknesses: ' 
+    Breakable_Parts = 'Breakable Parts: '
+    Rare_Materials = 'Rare Materials: '
+    Biology1 = ''
+    Biology2 = ''
+    Biology3 = ''
+if(MonsterData=5)
+    Monster_Name = 'Monster: '
+    Elements = 'Elements: '
+    Aliments = 'Aliments: ' 
+    Weaknesses = 'Weaknesses: ' 
+    Breakable_Parts = 'Breakable Parts: '
+    Rare_Materials = 'Rare Materials: '
+    Biology1 = ''
+    Biology2 = ''
+    Biology3 = ''
+ if(MonsterData=6)
+    Monster_Name = 'Monster: '
+    Elements = 'Elements: '
+    Aliments = 'Aliments: ' 
+    Weaknesses = 'Weaknesses: ' 
+    Breakable_Parts = 'Breakable Parts: '
+    Rare_Materials = 'Rare Materials: '
+    Biology1 = ''
+    Biology2 = ''
+    Biology3 = ''
+if(MonsterData=7)
+    Monster_Name = 'Monster: '
+    Elements = 'Elements: '
+    Aliments = 'Aliments: ' 
+    Weaknesses = 'Weaknesses: ' 
+    Breakable_Parts = 'Breakable Parts: '
+    Rare_Materials = 'Rare Materials: '
+    Biology1 = ''
+    Biology2 = ''
+    Biology3 = '' 
+if(MonsterData=8)
+    Monster_Name = 'Monster: '
+    Elements = 'Elements: '
+    Aliments = 'Aliments: ' 
+    Weaknesses = 'Weaknesses: ' 
+    Breakable_Parts = 'Breakable Parts: '
+    Rare_Materials = 'Rare Materials: '
+    Biology1 = ''
+    Biology2 = ''
+    Biology3 = ''    
+    if(MonsterData=9)
+    Monster_Name = 'Monster: '
+    Elements = 'Elements: '
+    Aliments = 'Aliments: ' 
+    Weaknesses = 'Weaknesses: ' 
+    Breakable_Parts = 'Breakable Parts: '
+    Rare_Materials = 'Rare Materials: '
+    Biology1 = ''
+    Biology2 = ''
+    Biology3 = ''
+    if(MonsterData=10)
+    Monster_Name = 'Monster: '
+    Elements = 'Elements: '
+    Aliments = 'Aliments: ' 
+    Weaknesses = 'Weaknesses: ' 
+    Breakable_Parts = 'Breakable Parts: '
+    Rare_Materials = 'Rare Materials: '
+    Biology1 = ''
+    Biology2 = ''
+    Biology3 = ''
+    
+print(Monster_Name)
+print(Elements)
+print(Aliments)
+print(Weaknesses)
+print(Breakable_Parts)
+print(Rare_Materials)
+print(Biology1)
+print(Biology2)
+print(Biology3)   
