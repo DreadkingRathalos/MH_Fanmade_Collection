6ee0568792fdf8013fcf985b4a1bb59646d4a329
DreadkingRathalos <108301431+DreadkingRathalos@users.noreply.github.com>
2022-07-20 15:55:53 -0500

    Update Locale_Information.py

--- Locale_Information.py
+++ Locale_Information.py
@@ -1,14 +1,49 @@
 Number=input('Please Enter a number to learn a Locale')
 if(Number == 1):
+  Locale = Vanished Town
+  Locale_Info1 = 'A once vibrant town that has been abandoned, and The reason of its demised is in the center of the town. The citizens of said former town talk of a mysterious Elder Dragon.'
+  Locale_Info2 = 'Various Buldings surounding the locale are often have a rare critter inside thatcan be used to hunt monsters.'
+  Locale_Info3 = 'There are various tunnel monsters can use in order to get to one area to another very quickly as well as objects that both you and the monster can take advantage of.'
+if(Number == 2):
+  Locale = 'Schorching Forest'
+  Locale_Info1 = 'A forest constantly set a flame by various monsters, and as a result, the trees become very harder to burn.'
+  Locale_Info2 = ''
+  Locale_Info3 = '' 
+if(Number == 3):
+  Locale = ''
+  Locale_Info1 = ''
+  Locale_Info2 = ''
+  Locale_Info3 = ''
+if(Number == 4):
+  Locale = ''
+  Locale_Info1 = ''
+  Locale_Info2 = ''
+  Locale_Info3 = ''
+if(Number == 5):
+  Locale = ''
+  Locale_Info1 = ''
+  Locale_Info2 = ''
+  Locale_Info3 = ''
+if(Number == 6):
+  Locale = ''
+  Locale_Info1 =
+  Locale_Info2 =
+  Locale_Info3 =
+if(Number == 7):
   Locale = 
   Locale_Info1 =
   Locale_Info2 =
   Locale_Info3 =
-  
-
-  
-  
-  
+if(Number == 8):
+  Locale = 
+  Locale_Info1 =
+  Locale_Info2 =
+  Locale_Info3 =
+if(Number == 9):
+  Locale = 
+  Locale_Info1 =
+  Locale_Info2 =
+  Locale_Info3 =
   
   print(Locale)
 print(Locale_Info1)
