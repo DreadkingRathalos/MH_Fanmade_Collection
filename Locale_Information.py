3c8a6e98909a00a0c3875a1cf80af71cc087f2f4
DreadkingRathalos <108301431+DreadkingRathalos@users.noreply.github.com>
2022-07-27 14:38:19 -0500

    Update Locale_Information.py

--- Locale_Information.py
+++ Locale_Information.py
@@ -5,12 +5,12 @@ if(Number == 1):
   Locale_Info2 = 'Various Buldings surounding the locale are often have a rare critter inside thatcan be used to hunt monsters.'
   Locale_Info3 = 'There are various tunnel monsters can use in order to get to one area to another very quickly as well as objects that both you and the monster can take advantage of.'
 if(Number == 2):
-  Locale = 'Schorching Forest'
+  Locale = 'Scorching Forest'
   Locale_Info1 = 'A forest constantly set a flame by various monsters, and as a result, the trees become very harder to burn.'
   Locale_Info2 = ''
   Locale_Info3 = '' 
 if(Number == 3):
-  Locale = ''
+  Locale = 'Rotten Swamp'
   Locale_Info1 = ''
   Locale_Info2 = ''
   Locale_Info3 = ''
